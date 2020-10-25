c83fe6e8e7ab8f5bf8764a605f0c4d40ab238d99
sekoudosso82 <sekou.dosso82@gmail.com>
2020-10-24 21:55:08 -0400

    mutation with enumaration

--- schema.js
+++ schema.js
@@ -6,10 +6,16 @@ const schema = buildSchema(`
         id: ID
         firstName: String
         lastName: String
-        gender: String
+        gender: Gender
         age: Int
         language: String
         email: String
+    }
+
+    enum Gender {
+        MALE
+        FEMALE
+        OTHER
     }
 
     type Query {
@@ -20,7 +26,7 @@ const schema = buildSchema(`
         id: ID
         firstName: String!
         lastName: String
-        gender: String
+        gender: Gender
         age: Int
         language: String
         email: String
